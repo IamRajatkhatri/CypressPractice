302f7d8acd35a1d8018936f1dfb8f490d316db7b
Rajat khatri <rajat.khatri.2609@gmail.com>
2024-04-30 13:17:43 +0000

    Update todo-test.cy.ts

--- cypress/e2e/1-getting-started/todo-test.cy.ts
+++ cypress/e2e/1-getting-started/todo-test.cy.ts
@@ -1,8 +1,8 @@
 describe("test suite", function () {
-    it("homepage should display", function () {
-        cy.visit("https://qa.bitdelta.com/en/nomination")
+   // it("homepage should display", function () {
+   //     cy.visit("https://qa.bitdelta.com/en/nomination")
       
-    })
+   // })
 
     it("Add nominee button should be clicked",() => {
         cy.visit("https://qa.bitdelta.com/en/nomination")
@@ -10,14 +10,14 @@ describe("test suite", function () {
     })
 })
 
-// describe('Tutorialspoint Test', function () {
+ describe('Tutorialspoint Test', function () {
 //     // test case
-//     it('Scenario 1', function (){
+     it('Scenario 1', function (){
 //        // launch URL
-//        cy.visit("https://www.letskodeit.com/practice");
+        cy.visit("https://www.letskodeit.com/practice");
 //        // show hidden element with invoke
-//        cy.get('div.mouse-hover-content').invoke('show');
+        cy.get('div.mouse-hover-content').invoke('show');
 //        //click hidden element
-//        cy.contains('Top').click();
-//     });
-//  });
+        cy.contains('Top').click();
+    });
+  });
