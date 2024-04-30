4d4e15090af66d9ff0d51a69d153db8c90b082fc
Rajat Khatri <rajat.khatri.2609@gmail.com>
2024-04-30 16:28:25 +0530

    updated code

--- cypress/e2e/1-getting-started/todo-test.cy.ts
+++ cypress/e2e/1-getting-started/todo-test.cy.ts
@@ -1,14 +1,26 @@
-describe("test suite", function () {
-    it("homepage should display", function () {
-        cy.visit("https://qa.bitdelta.com/en/nomination")
-      
-    })
 
-    it("Add nominee button should be clicked",() => {
-        cy.visit("https://qa.bitdelta.com/en/nomination")
-        cy.get('.css-48oap0 > .chakra-button').click
+describe("test suite", () => {
+    it("Login Sucessfully", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.title().should('eq', 'OrangeHRM')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.oxd-button').click
     })
 })
+
+
+// describe("test suite", function () {
+//     it("homepage should display", function () {
+//         cy.visit("https://qa.bitdelta.com/en/nomination")
+      
+//     })
+
+//     it("Add nominee button should be clicked",() => {
+//         cy.visit("https://qa.bitdelta.com/en/nomination")
+//         cy.get('.css-48oap0 > .chakra-button').click
+//     })
+// })
 
 // describe('Tutorialspoint Test', function () {
 //     // test case
