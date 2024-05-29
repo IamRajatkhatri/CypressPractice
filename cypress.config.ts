a03b2ea43c515565b31172d724eebec9a8537b7c
Rajat Khatri <rajat.khatri.2609@gmail.com>
2024-05-29 18:50:07 +0530

    Added parallel and visual testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,25 +1,24 @@
 import { defineConfig } from "cypress";
-import { configureVisualRegression } from "cypress-visual-regression/dist/plugin";
+//import { configureVisualRegression } from "cypress-visual-regression/dist/plugin";
 const { allureCypress } = require("allure-cypress/reporter");
 export default defineConfig({
   e2e: {
-    env: {
-      
-      visualRegressionType: "regression",
-      visualRegressionBaseDirectory: "cypress/snapshot/base",
-      visualRegressionDiffDirectory: "cypress/snapshot/diff",
-      visualRegressionGenerateDiff: "always",
-      visualRegressionFailSilently: true,
-      updateSnapshots: true,
+    // env: {
+    //   visualRegressionType: "regression",
+    //   visualRegressionBaseDirectory: "cypress/snapshot/base",
+    //   visualRegressionDiffDirectory: "cypress/snapshot/diff",
+    //   visualRegressionGenerateDiff: "always",
+    //   visualRegressionFailSilently: true,
+    //   updateSnapshots: true,
     
     baseUrl: "https://learn.cypress.io/",
     supportFile: false,
     specPattern: "cypress/e2e/**/*.cy.ts",
-    },
+    //},
  },
     screenshotsFolder: "./cypress/snapshots/actual",
     setupNodeEvents(on, config) {
-      configureVisualRegression(on);
+     // configureVisualRegression(on);
        allureCypress(on);
     },
   
